53a73eacab730ce204e7aae6e163240c1559f3cd
Владислав <vlad2004ki@gmail.com>
2024-08-07 12:01:43 +0300

    settings view update

--- My Walks/My Walks/Views/SettingsView.swift
+++ My Walks/My Walks/Views/SettingsView.swift
@@ -3,11 +3,24 @@ import SwiftUI
 struct SettingsView: View {
     @Environment(\.dismiss) private var dismiss
     @AppStorage("isDarkModeOn") private var isDarkModeOn = false
+    @State private var measurementSystem = "Imperial"
+    @State private var appLanguage = "English"
     
     var body: some View {
         NavigationStack {
             Form {
-                Toggle("Dark Mode", isOn: $isDarkModeOn)
+                Section("Main settings"){
+                    Picker("Measurement system", selection: $measurementSystem) {
+                        Text("Metric")
+                        Text("Imperial")
+                    }
+                    Picker("App language", selection: $appLanguage) {
+                        Text("English")
+                    }
+                }
+                Section("App deisgn") {
+                    Toggle("Dark Mode", isOn: $isDarkModeOn)
+                }
             }
             .navigationTitle("Settings")
         }
